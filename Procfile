22614a4e0f1e0870801d73e196223d76ed8f7043
Kushagra Sinha <sinha.kushagra@gmail.com>
2012-11-11 19:43:35 +0530

    Static server separated. App server changed to gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fests.wsgi -b 0.0.0.0:$PORT -w 3
--- fests/settings.py
+++ fests/settings.py
@@ -1,7 +1,9 @@
 # Django settings for fests project.
 
-# Heroku specific
 import dj_database_url
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -62,11 +64,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../../fests-static/static/assets/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://fests-static.herokuapp.com/assets/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -127,6 +129,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 
     'tastypie',
+    'gunicorn',
 
     'api',
 )
