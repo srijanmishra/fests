2877650a7ca66b881187774739ad95f3920e1221
Kushagra Sinha <sinha.kushagra@gmail.com>
2012-11-12 13:47:32 +0530

    Static files structure modified

--- fests/settings.py
+++ fests/settings.py
@@ -68,14 +68,14 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '../fests-static/static/assets/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../fests-static/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 if DEBUG:
     STATIC_URL = '/static/'
 else:
-    STATIC_URL = 'http://fests-static.herokuapp.com/assets/'
+    STATIC_URL = 'http://fests-static.herokuapp.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
